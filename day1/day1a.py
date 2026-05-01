981f721f0cf098a6cf8f9e1cc46c9b8bea211d18
gabrielsoares01 <gabrielldasilva969@usp.br>
2026-05-01 20:05:50 -0300

    first commit

---
+++ day1/day1a.py
@@ -0,0 +1,25 @@
+def main():
+    finalState = 50
+    password = 0
+
+    print(f"The dial starts by pointing at ", finalState)
+
+    with open('inputa.txt', 'r') as input:
+        inputRotations = input.read().splitlines()
+
+    for rotation in inputRotations:
+        movement = int(rotation[1:])
+
+        if rotation.startswith("L"):
+            movement *= -1
+        
+        finalState = (finalState + movement) % 100
+
+        password += 1 if finalState == 0 else 0
+
+        print(f"The dial is rotated {rotation} to point at {finalState}")
+
+    print("\npassword:", password)
+
+if __name__ == "__main__":
+    main()
